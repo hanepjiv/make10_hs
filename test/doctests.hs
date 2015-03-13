d7e38f66be6a7f2104e302b2a057f6d3cbc497d5
hanepjiv <hanepjiv@gmail.com>
2015-03-13 23:10:40 +0900

    make10.cabal

--- test/doctests.hs
+++ test/doctests.hs
@@ -9,11 +9,23 @@ module Main where
 -- -----------------------------------------------------------------------------
 import Prelude
 
+import Control.Arrow    ( Kleisli(..)
+                        , runKleisli
+                        , (>>>)
+                        )
+
 import qualified Test.DocTest as DocTest
 -- =============================================================================
 -- -----------------------------------------------------------------------------
 main :: IO ()
-main = DocTest.doctest [ "-packageghc"
-                       , "-isrc"
-                       , "src/Make10.hs"
-                       ]
+main =  flip runKleisli () $
+        (Kleisli $ const $ DocTest.doctest [ "-packageghc"
+                                           , "-isrc"
+                                           , "src/Make10.hs"
+                                           ])
+        >>>
+        (Kleisli $ const $ DocTest.doctest [ "-packageghc"
+                                           , "-isrc"
+                                           , "-iexample/make10"
+                                           , "example/make10/Main.hs"
+                                           ])
