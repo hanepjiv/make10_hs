651f6f4f6a97a34fd8795ad47f042f30247a9050
hanepjiv <hanepjiv@gmail.com>
2025-09-23 15:16:05 +0900

    fixes test

--- test/spec/Main.hs
+++ test/spec/Main.hs
@@ -2,8 +2,7 @@ module Main where
 
 import Prelude
 import Test.Hspec.Runner
-import Test.Hspec.Formatters
 import qualified Spec
 
 main :: IO ()
-main = hspecWith defaultConfig {configFormatter = Just progress} Spec.spec
+main = hspec Spec.spec
